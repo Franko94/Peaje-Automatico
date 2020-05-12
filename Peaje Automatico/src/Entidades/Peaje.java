a5045149e6fdcef7ab8bade8653555657a8f0724
Mateo de Sierra <themateods@gmail.com>
2020-05-12 14:46:28 -0300

    Clase peaje y metodo cobrar

---
+++ Peaje Automatico/src/Entidades/Peaje.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+import java.util.LinkedList;
+import peaje.automatico.Entidades.Vehiculo;
+
+/**
+ *
+ * @author Teo
+ */
+public class Peaje {
+    
+    private String nombre;
+    private int cantCabinas;
+    LinkedList listaCabinas;
+    private int totalDinero = 0;
+    
+    
+    public void Peaje(String nombre, int numCabinas){
+        this.nombre = nombre;
+        cantCabinas = numCabinas;
+        listaCabinas = new LinkedList();
+    }
+    
+    
+    public void cobrar(Vehiculo vehiculo){
+        totalDibero += this.fijarsePrecio(vehiculo);
+        vehiculo.setHoraSalida(ahora);
+    }
+    
+    public int fijarsePrecio(Vehiculo vehiculo){
+        if (vehiculo.getTipo == "auto"){
+            return 10;
+        }
+        else if (vehiculo.getTipo == "camion"){
+            return 20;
+        }
+        else if (vehiculo.getTipo == "moto"){
+            return 15;
+        }
+    }
+    
+}
