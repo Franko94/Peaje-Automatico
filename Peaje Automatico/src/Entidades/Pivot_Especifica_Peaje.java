dfb3cb8d8837e95fcbdb7dac29ed7ad42ce2e9c0
Franko94 <franco.gai.94@gmail.com>
2020-05-15 20:45:23 -0300

    Create Pivot_Especifica_Peaje.java
    creacion de la clase de segundo pivot

---
+++ Peaje Automatico/src/Entidades/Pivot_Especifica_Peaje.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+import java.util.Queue;
+
+/**
+ *
+ * @author fran_
+ */
+public class Pivot_Especifica_Peaje implements Runnable {
+
+    public static Queue <CabinaPeaje> colaPeaje;
+    public static Queue <Vehiculo> colaNormales;
+    public static Queue <Vehiculo> colaEspeciales;
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    private void distribuir(){
+        
+    }
+}
