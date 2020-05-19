5c8970caf52b0a54d7093074949b3cb048a3d8c5
unknown <agustinpicos@gmail.com>
2020-05-19 16:35:35 -0300

    Para entregar

--- Peaje_Automatico/src/Logger/Logger.java
+++ Peaje_Automatico/src/Logger/Logger.java
@@ -9,67 +9,45 @@ import Entidades.Vehiculo;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import static java.lang.Thread.sleep;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.logging.Level;
 
 /**
  *
  * @author fran_
  */
 public class Logger {
-    
-    private static final String RUTA = "src\\Escenarios\\log.csv";    
-    public static Queue<String> cola = new LinkedList<>();
-    
+
+    private static final String RUTA = "src\\Escenarios\\log.csv";
+
     public static void log(String linea) {
-//        FileWriter fw;
-//        try {
-//            fw = new FileWriter(RUTA, true);
-//            BufferedWriter bw = new BufferedWriter(fw);
-//
-//            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
-//            LocalDateTime now = LocalDateTime.now();  
-//
-//            String lineaActual = linea + "," + now;
-//            bw.write(lineaActual);
-//            bw.newLine();
-//            bw.close();
-//            fw.close();
-//        } catch (IOException e) {
-//            System.out.println("Error al escribir el archivo "
-//                    + RUTA);
-//            e.printStackTrace();
-//        }
-        Logger.addLinea(linea);
+        FileWriter fw;
+        try {
+            sleep(3);
+        } catch (InterruptedException ex) {
+            java.util.logging.Logger.getLogger(Logger.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            fw = new FileWriter(RUTA, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+            LocalDateTime now = LocalDateTime.now();
+            
+            
+           String lineaActual = linea + "," + now;
+            bw.write(lineaActual);
+            bw.newLine();
+            bw.close();
+            fw.close();
+        } catch (IOException e) {
+            System.out.println("Error al escribir el archivo "
+                    + RUTA);
+            e.printStackTrace();
+        }
     }
 
-    public static void addLinea(String linea) {
-        //Logger.cola.add(linea);
-    }
-    
-    public static void guardarLogs() {
-//        FileWriter fw;
-//        try {
-//            fw = new FileWriter(RUTA, true);
-//            BufferedWriter bw = new BufferedWriter(fw);
-//            while (!Logger.cola.isEmpty()) {
-//                
-//                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");                
-//                LocalDateTime now = LocalDateTime.now();                
-//                String linea = cola.poll();
-//                String lineaActual = linea + "," + now;
-//                bw.write(lineaActual);
-//                bw.newLine();
-//            }
-//            bw.close();
-//            fw.close();
-//        } catch (IOException e) {
-//            System.out.println("Error al escribir el archivo "
-//                    + RUTA);
-//            e.printStackTrace();
-//        }
-    }
-    
 }
