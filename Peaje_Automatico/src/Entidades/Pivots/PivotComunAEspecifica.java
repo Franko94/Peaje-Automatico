bd80e57e77de5a46894405c2e5ce56cf1156dc3d
AgustinPicos <39628989+AgustinPicos@users.noreply.github.com>
2020-06-17 22:59:59 -0300

    Update PivotComunAEspecifica.java

--- Peaje_Automatico/src/Entidades/Pivots/PivotComunAEspecifica.java
+++ Peaje_Automatico/src/Entidades/Pivots/PivotComunAEspecifica.java
@@ -52,7 +52,7 @@ public class PivotComunAEspecifica extends Thread {
             if (reloj.nuevoCiclo(id_de_hilo) != true) {
                 try {
                     synchronized (reloj) {
-                        reloj.wait(10);
+                        reloj.wait(1);
                     }
                 } catch (InterruptedException e) {
                     e.printStackTrace();
