1a11fe1038903288b0bdba8d3dac47135afde130
AgustinPicos <39628989+AgustinPicos@users.noreply.github.com>
2020-06-17 22:58:44 -0300

    Update HabilitadorDeCabinas.java

--- Peaje_Automatico/src/Entidades/HabilitadorDeCabinas.java
+++ Peaje_Automatico/src/Entidades/HabilitadorDeCabinas.java
@@ -31,7 +31,7 @@ public class HabilitadorDeCabinas extends Thread {
             if (reloj.nuevoCiclo(id_de_hilo) != true) {
                 try {
                     synchronized (reloj) {
-                        reloj.wait(10);
+                        reloj.wait(1);
                     }
                 } catch (InterruptedException e) {
                 }
