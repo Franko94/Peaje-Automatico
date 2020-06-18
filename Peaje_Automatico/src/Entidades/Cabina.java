273891bbc24205a665cd4c199cd89359c2b5cbe3
AgustinPicos <39628989+AgustinPicos@users.noreply.github.com>
2020-06-17 22:58:22 -0300

    Update Cabina.java

--- Peaje_Automatico/src/Entidades/Cabina.java
+++ Peaje_Automatico/src/Entidades/Cabina.java
@@ -47,7 +47,7 @@ public class Cabina extends Thread {
                 try {
                     synchronized (reloj) {
 
-                        reloj.wait(10);
+                        reloj.wait(1);
                     }
                 } catch (InterruptedException e) {
                 }
